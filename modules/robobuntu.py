61bd3d75a4d1f13a92fc1740094af7da7dd3abdb
Thomas Hahn <Thahn@tcbtech.com>
2013-01-11 09:27:03 +0000

    geodudes

--- modules/robobuntu.py
+++ modules/robobuntu.py
@@ -15,17 +15,22 @@ def robobuntu(phenny, input):
 robobuntu.priority = 'low'
 robobuntu.rule = ".*ubuntu.*"
 
-r'(\w+)\W(ten|10)\W(\w+)', re.IGNORECASE)
 def pokemans(phenny, input):
 	# You can't X ten Y!
 	x, ten, y = pokere.search(input).groups()
 	phenny.say("You can't %s ten %ss!" % (x, y))
 	return
 
-pokemans.rule = r'.*(\w*)\W(ten|10)\W(\w*).*'
+pokemans.rule = r'(\w+)\W(ten|10)\W(\w+)'
 pokere = re.compile(pokemans.rule, re.IGNORECASE)
 pokemans.priority = 'low'
 
+def geodudes(phenny, input):
+	phenny.say("6 geodudes, can't lose!")
+	return
+geodudes.rule = r'(6|six) geodues'
+
 if __name__ == '__main__': 
 	print __doc__.strip()
+
 
