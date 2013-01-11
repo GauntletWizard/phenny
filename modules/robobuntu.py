585abcfd83e5c1880bd5851a509b101141e891b3
Thomas Hahn <Thahn@tcbtech.com>
2013-01-11 09:08:35 +0000

    Needs api docs. Gods.

--- modules/robobuntu.py
+++ modules/robobuntu.py
@@ -9,19 +9,24 @@ Copyright 2013 Google Inc
 import re
 
 def robobuntu(phenny, input):
-				#oo-BUN-too
-				phenny.say("oo-BOON-too!")
-				print "called: ", input
-robobuntu.priority = 'high'
-robobuntu.commands = ["ubuntu"]
-robobuntu.r_commands = [re.compile(r'.*ubuntu.*', re.IGNORECASE)]
+  #oo-BUN-too
+	phenny.say("oo-BOON-too!")
+	print "called: ", input
+	return
+robobuntu.priority = 'low'
+robobuntu.rule = ".*ubuntu.*"
 
 pokere = re.compile(r'(\w*)\W(ten|10)\W(\w*)', re.IGNORECASE)
 def pokemans(phenny, input):
-				# You can't X ten Y!
-				print "pokemans"
-				x, ten, y = pokere.search(input).groups()
-				phenny.say("You can't %s ten %s!" % (x, y))
-pokemans.rule = r'(\w*)\W(ten|10)\W(\w*)'
-pokemans.priority = 'high'
-pokemans.r_commands = [pokere]
+	# You can't X ten Y!
+	print "pokemans"
+	x, ten, y = pokere.search(input).groups()
+	phenny.say("You can't %s ten %s!" % (x, y))
+	return
+
+pokemans.rule = r'.*(\w*)\W(ten|10)\W(\w*).*'
+pokemans.priority = 'low'
+
+if __name__ == '__main__': 
+	print __doc__.strip()
+
